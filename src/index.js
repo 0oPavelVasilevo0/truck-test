4a57f92103dc7611122ca61b9664cd6c2b90c8c0
Pavel Vasilev <karterpahan@gmail.com>
2023-11-15 17:35:54 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// import serviceWorker from './../public/pwabuilder-sw';
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,3 +19,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// serviceWorker();
