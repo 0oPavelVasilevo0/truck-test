0e9b19d6eefe86d76e28a70fae96e586a8317bb9
Pavel Vasilev <karterpahan@gmail.com>
2023-12-29 14:30:38 +0100

    fix

--- src/components/projects/ProjectOne.jsx
+++ src/components/projects/ProjectOne.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './projects_style.css';
+import { useTranslation } from 'react-i18next';
 
 const ProjectOne = () => {
+
+    const { t } = useTranslation();
+
     return (
         <div className='project_01'>
             <div className='list-project'>
                 <ol>
                     <ul className='list_project_ul'>
-                        <li>Ремонт двигателя</li>
-                        <li>Ремонт ходовки</li>
-                        <li>Тормозная система</li>
-                        <li>Ремонт рулевой сиситемы</li>
-                        <li>Ремонт КПП</li>
-                        <li>Ремонт редуктора</li>
-                        <li>Ремонт электрики</li>
-                        <li>Ремонт пневмосистем</li>
+                        <li>{t('projects.one.engine')}</li>
+                        <li>{t('projects.one.hood')}</li>
+                        <li>{t('projects.one.brake')}</li>
+                        <li>{t('projects.one.steering')}</li>
+                        <li>{t('projects.one.transmission')}</li>
+                        <li>{t('projects.one.gearbox')}</li>
+                        <li>{t('projects.one.electrical')}</li>
+                        <li>{t('projects.one.pneumatic')}</li>
                     </ul>
                 </ol>
             </div>
