ae8be058f72a88cfad27edcc6547384603c728ad
Pavel Vasilev <karterpahan@gmail.com>
2024-01-11 00:15:39 +0100

    creating a burger-menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import BtnDarkMode from '../btnDarkMode/BtnDarkMode';
 import "./styles_Navbar.css";
 import ChangeLang from '../ChangeLang/ChangeLang';
 import { useTranslation } from 'react-i18next';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaRegWindowClose } from "react-icons/fa";
+
+// const Navbar = () => {
+
+//     const activeLink = 'nav-list__link nav-list__link--active';
+//     const normalLink = 'nav-list__link';
+//     const { t } = useTranslation();
+
+//     return (
+//         <nav className="nav">
+//             <div className="container">
+//                 <div className="nav-row">
+//                     <NavLink to="/" className="logo"><strong>{t('navbar_title')}</strong> {t('navbar_title/')}</NavLink>
+//                     <BtnDarkMode />
+//                     <ChangeLang />
+//                     <ul className="nav-list">
+//                         <li className="nav-list__item">
+//                             <NavLink to="/" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list1')}</NavLink>
+//                         </li>
+//                         <li className="nav-list__item">
+//                             <NavLink to="/projects" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list2')}</NavLink>
+//                         </li>
+//                         <li className="nav-list__item">
+//                             <NavLink to="/contacts" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list3')}</NavLink>
+//                         </li>
+//                     </ul>
+
+//                 </div>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// export default Navbar
+
 
 const Navbar = () => {
 
-    const activeLink = 'nav-list__link nav-list__link--active';
-    const normalLink = 'nav-list__link';
     const { t } = useTranslation();
 
+    const activeLink = 'nav-list__link nav-list__link--active';
+    const normalLink = 'nav-list__link';
+
+        // const activeMenu = 'button-icon--active';
+        // const normalMenu = 'button-icon';
+
+    const [isOpen, setOpen] = useState();
+
+    const toggleMenu = () => {
+        setOpen(!isOpen);
+    };
+
     return (
-        <nav className="nav">
-            <div className="container">
-                <div className="nav-row">
-                    <NavLink to="/" className="logo"><strong>{t('navbar_title')}</strong> {t('navbar_title/')}</NavLink>
-                    <BtnDarkMode />
-                    <ChangeLang />
-                    <ul className="nav-list">
-                        <li className="nav-list__item">
-                            <NavLink to="/" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list1')}</NavLink>
-                        </li>
-                        <li className="nav-list__item">
-                            <NavLink to="/projects" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list2')}</NavLink>
-                        </li>
-                        <li className="nav-list__item">
-                            <NavLink to="/contacts" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list3')}</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <div className="navbar">
+            <span className="navbar__logo">
+                <NavLink to="/" className="logo"><strong>{t('navbar_title')}</strong> {t('navbar_title/')}</NavLink>
+            </span>
+            <nav className={`navbar__nav ${isOpen ? "active" : ""}`}>
+                {/* <button
+                    className='navbar__menu-close'
+                    onClick={() => setOpen(!isOpen)}
+                >
+                    <FaRegWindowClose className='button-icon' />
+                </button> */}
+                <ul className="navbar__nav-list">
+                    <li className="navbar__nav-item">
+                        <NavLink to="/" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list1')}</NavLink>
+                    </li>
+                    <li className="navbar__nav-item">
+                        <NavLink to="/projects" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list2')}</NavLink>
+                    </li >
+                    <li className="navbar__nav-item">
+                        <NavLink to="/contacts" className={({ isActive }) => isActive ? activeLink : normalLink}>{t('navbar_list3')}</NavLink>
+                    </li >
+                    <li className="navbar__nav-item">
+                        <ChangeLang />
+                    </li >
+                    <li className="navbar__nav-item">
+                        <BtnDarkMode />
+                    </li>
+                </ul >
+            </nav >
+            {/* <button
+                className="navbar__menu-button"
+                onClick={() => setOpen(!isOpen)}
+            >
+                <GiHamburgerMenu 
+                className="button-icon"
+                />
+            </button > */}
+            <button className='navbar__menu-button' onClick={toggleMenu}>
+                {isOpen ? (
+                    <FaRegWindowClose className='button-icon' />
+                ) : (
+                    <GiHamburgerMenu className='button-icon' />
+                )}
+            </button>
+                 {/* className={({ isActive }) => isActive ? activeMenu : normalMenu} */}
+        </div>
     )
 }
 
